a61141b00202a6546c35760f2d30bef11fb5fd61
fiannafail <fiannafail28@gmail.com>
2018-01-02 17:17:24 +0300

    make it better

---
+++ components/ScenariosSection.vue
@@ -0,0 +1,89 @@
+<template lang="pug">
+  section(class="scenarios-section")
+    div(class="wrapper")
+      div(class="covers")
+      div(class="text")
+        h2(class="section-headline") {{ language === 'ru' ? sectionsMeta.Scenarios.title : sectionsMeta.Scenarios.titleEN }}
+        p(class="red-text") {{ language === 'ru' ? sectionsMeta.Scenarios.subtitle : sectionsMeta.Scenarios.subtitleEN }}
+        p(class="section-text") {{ language === 'ru' ? sectionsMeta.Scenarios.text : sectionsMeta.Scenarios.textEN }}
+        p(class="section-text bold") АВТОР БОЛЕЕ 20 СЦЕНАРИЕВ, СРЕДИ КОТОРЫХ
+        ul(class="scenarios-list")
+          li(v-for="(item, index) in scenariosMovies") {{ language === 'ru' ? item.title : item.titleEN }}
+</template>
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data: () => ({
+
+  }),
+  computed: mapState({
+    sectionsMeta: 'sectionsMeta',
+    scenariosMovies: 'scenariosMovies',
+    language: 'language'
+  })
+}
+</script>
+<style lang="scss" scoped>
+@mixin red-text {
+  font-family: Open Sans;
+  font-size: 14px;
+  font-weight: 600;
+  color: #e02106;
+  text-transform: uppercase;
+}
+@mixin red-line {
+  margin-top: 18px;
+  display: block;
+  content: "";
+  height: 125px;
+  width: 2px;
+  background-color: #e02106;
+}
+.scenarios-section {
+  background-color: #181818;
+  padding: 130px 0;
+  position: relative;
+  .scenarios-list {
+    list-style-type: none;
+    color: #bbbbbb;
+    columns: 2;
+    line-height: 1.93;
+    li::before {
+      content: '一 '
+    }
+  }
+  &::before {
+    @include red-line;
+    margin-top: 0;
+    position: absolute;
+    left: calc(50% - 1px);
+    top: 0;
+  }
+  .wrapper {
+    width: 1180px;
+    margin: 0 auto;
+    display: flex;
+    .section-text {
+      font-family: Open Sans;
+      font-size: 14px;
+      line-height: 1.93;
+      margin: 30px 0;
+      color: #bbbbbb;
+      &.bold {
+        font-weight: 600;
+        color: #ffffff;
+      }
+    }
+    .red-text {
+      @include red-text;
+      margin-top: 15px;
+      margin-bottom: 15px;
+    }
+    & > * {
+      flex: 1 1;
+      margin-top: 10px;
+    }
+  }
+}
+</style>
---
+++ components/admin/ScenariosSection.vue
@@ -0,0 +1,116 @@
+<template lang="pug">
+  section(class="scenarios-section elevation-4")
+      h2(class="headline") Сценарии
+      v-container(grid-list-md)
+        v-layout
+          v-flex(xs3)
+            v-subheader Заголовок
+          v-flex(xs9)
+            v-text-field(v-model="SectionsMeta.Scenarios.title" required hide-details)
+            v-text-field(v-model="SectionsMeta.Scenarios.titleEN" required hide-details)
+        v-layout
+          v-flex(xs3)
+            v-subheader Подаголовок
+          v-flex(xs9)
+            v-text-field(v-model="SectionsMeta.Scenarios.subtitle" required hide-details)
+            v-text-field(v-model="SectionsMeta.Scenarios.subtitleEN" required hide-details)
+        v-layout
+          v-flex(xs3)
+            v-subheader Текст
+          v-flex(xs9)
+            v-text-field(v-model="SectionsMeta.Scenarios.text" textarea required hide-details)
+            v-text-field(v-model="SectionsMeta.Scenarios.textEN" textarea required hide-details)
+        v-btn(color="primary" @click="editSectionMeta('Scenarios')") Сохранить изменения
+        v-divider
+        h2 Фильмы
+        v-container(grid-list-md)
+          v-layout
+            v-flex(xs6)
+              v-layout(v-for="(item, index) in scenariosMovies" :key="index")
+                transition(name="fade")
+                  div(class="photo-overlay" v-if="showRemovedScenarios(index) === true")
+                v-flex(xs10 class="body-2" d-flex) {{ item.title }}
+                v-flex(xs2)
+                  v-btn(flat icon color="red" @click="removeScenario(item, index)")
+                    v-icon close
+        v-layout
+          v-flex(xs3 d-flex)
+            v-subheader Название фильма
+          v-flex(xs6)
+            v-text-field(v-model="movie.title" required hide-details label="Название")
+            v-text-field(v-model="movie.titleEN" required hide-details label="Название (EN)")
+          v-flex(d-flex)
+            v-btn(color="primary" @click="addScenario") Добавить
+</template>
+<script>
+import { mapState } from 'vuex'
+import { baseElementUpdate, addElement } from '~/plugins/functions.js'
+//  import { database } from '~/plugins/firebase-client-init.js'
+
+export default {
+  data: () => ({
+    revomedScenarios: [],
+    movie: {
+      title: null,
+      titleEN: null
+    },
+    SectionsMeta: {
+      Scenarios: {
+        title: null,
+        subtitle: null,
+        text: null,
+        titleEN: null,
+        subtitleEN: null,
+        textEN: null
+      }
+    }
+  }),
+  methods: {
+    async removeScenario (item, index) {
+      //  await database.ref('Scenarios').child(item.key).remove()
+      this.revomedScenarios.push(index)
+    },
+    async addScenario () {
+      await addElement('Scenarios', this.movie)
+      this.$store.dispatch('updateScenariosMovies')
+    },
+    showRemovedScenarios (index) {
+      const array = this.revomedScenarios
+      for (let i = 0; i < array.length; i++) {
+        if (array[i] === index) {
+          return true
+        }
+      }
+    },
+    editSectionMeta (type) {
+      baseElementUpdate('Texts', type, this.SectionsMeta.Scenarios)
+    }
+  },
+  created () {
+    this.SectionsMeta = this.sectionsMeta
+  },
+  computed: mapState({
+    sectionsMeta: 'sectionsMeta',
+    scenariosMovies: 'scenariosMovies'
+  })
+}
+</script>
+<style lang="scss" scoped>
+.scenarios-section {
+  h2 {
+    margin: 16px 0;
+  }
+}
+button:not(.btn--flat) {
+  margin: 25px 0;
+}
+.layout {
+  position: relative;
+  .flex.xs6 > .layout {
+    border-bottom: 1px solid #dcdcdc;
+  }
+  .d-flex {
+    align-items: center;
+  }
+}
+</style>

6fdc13a1531d2d8fd3bdc211076fbdbe19f15d7b
fiannafail <fiannafail28@gmail.com>
2018-01-11 16:12:34 +0300

    make it better

---
+++ components/MediaSection.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  section(class="media-section")
+    p(v-lang.massMedia)
+    h1(class="section-headline" v-lang.written)
+    no-ssr
+      carousel(:perPage="4")
+        slide(v-for="(item, index) in Issues" :key="index")
+          div
+            a(:href="item.url")
+              img(v-lazy="item.image")
+              div(class="meta-info")
+                h3 {{ language === 'ru' ? item.title : item.titleEN }}
+                p {{ language === 'ru' ? item.magazine : item.magazineEN }}
+</template>
+<script>
+import { Carousel, Slide } from 'vue-carousel'
+import { mapState } from 'vuex'
+import NoSSR from 'vue-no-ssr'
+
+export default {
+  computed: mapState({
+    Issues: 'issues'
+  }),
+  components: {
+    'no-ssr': NoSSR,
+    Carousel,
+    Slide
+  }
+}
+</script>
+<<style lang="scss">
+	
+</style>
